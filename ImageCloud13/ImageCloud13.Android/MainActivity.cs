19d58bf214d888f53671e38fc89132530f20f948
Peter N Jenkin <peternjenkin@gmail.com>
2019-06-04 11:10:23 +0100

    13-119 Image picking with fix for permissions from Q&A/forum

--- ImageCloud13/ImageCloud13.Android/MainActivity.cs
+++ ImageCloud13/ImageCloud13.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.CurrentActivity;
 
 namespace ImageCloud13.Droid
 {
@@ -19,7 +20,16 @@ namespace ImageCloud13.Droid
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);    // as per https://www.udemy.com/complete-xamarin-developer-course-ios-and-android/learn/lecture/9330950#questions/7073406
             LoadApplication(new App());
         }
-    }
+
+        // as per https://forums.xamarin.com/discussion/comment/343777/#Comment_343777 and https://www.udemy.com/complete-xamarin-developer-course-ios-and-android/learn/lecture/9330950#questions/7073406
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+    }    
 }
--- ImageCloud13/ImageCloud13/MainPage.xaml.cs
+++ ImageCloud13/ImageCloud13/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,36 @@ namespace ImageCloud13
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void SelectImageButton_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            // Cross-platform initialise to use native technology (Initialize is async)
+
+            // Is device able to take photos?
+            if (!CrossMedia.Current.IsPickPhotoSupported)                           // options also pick|take photo|video
+            {
+                await DisplayAlert("Error","This is not supported on your device","Ok");
+                return;         // give up here
+            }
+
+            var mediaOptions = new PickMediaOptions
+            {
+                PhotoSize = PhotoSize.Medium       // medium is 50%, small is 25%; width/height, full & custom available
+            };      // initialize variable property(ies)
+
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);      // options also pick|take photo|video
+            // get the file (in this case, pick image)
+
+            if (selectedImageFile == null)          // standard check if something is really existent (maybe some glitch or deletion)
+            {
+                await DisplayAlert("Error", "There was an error when trying to get the image", "Ok");
+                return;                             // and give up
+            }
+
+            selectedImage.Source = ImageSource.FromStream(() => selectedImageFile.GetStream());
+            // TODO: why lambda syntax here? - perhaps as could be more complex than just the 1 statement here (in curly braces)
         }
     }
 }
